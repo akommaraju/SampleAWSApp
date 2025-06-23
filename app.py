952737db31eee6a8a9b5762f65ab02fffbeabda5
akommaraju <akommaraju@yahoo.com>
2025-06-23 18:37:44 -0400

    Test Changes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return {"message": "Hello, Anil! Welcome to Flask in AWS."}
+    return {"message": "Hello, Anil! Welcome to Flask in AWS. Made with ❤️ by Anil!"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
